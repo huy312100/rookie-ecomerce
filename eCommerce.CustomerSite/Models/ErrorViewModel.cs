bc306b7915cd2a92e0d16b9f8061c4700f69e82f
Nguyễn Ngọc Đức Huy <huy312100@gmail.com>
2022-02-18 19:19:07 +0700

    Initial project

---
+++ eCommerce.CustomerSite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace eCommerce.CustomerSite.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
