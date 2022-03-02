4811e6bf133e377e0c4ed7c14843e580b0021c21
Nguyễn Ngọc Đức Huy <huy312100@gmail.com>
2022-03-03 01:21:06 +0700

    Add login ui

---
+++ eCommerce.CustomerSite/Controllers/AuthenController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace eCommerce.CustomerSite.Controllers
+{
+    public class AuthenController : Controller
+    {
+        private readonly ILogger<AuthenController> _logger;
+
+        public AuthenController(ILogger<AuthenController> logger)
+        {
+            _logger = logger;
+        }
+
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
+
