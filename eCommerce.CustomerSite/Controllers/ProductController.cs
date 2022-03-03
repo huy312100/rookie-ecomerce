ec47ba9293554ef3e990636bfd4f3c236ffc5927
Nguyễn Ngọc Đức Huy <huy312100@gmail.com>
2022-03-03 21:32:48 +0700

    Add detail of product ui

---
+++ eCommerce.CustomerSite/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace eCommerce.CustomerSite.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
+
