f5760155be8f8f5239534caf6d5436f27a0d862c
Nguyễn Ngọc Đức Huy <huy312100@gmail.com>
2022-03-11 01:42:53 +0700

    Install all essential packages for admin site

--- ecommerce-admin-site/src/App.js
+++ ecommerce-admin-site/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { lazy } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+import { ToastContainer } from './utils/toast';
+import AccessibleNavigationAnnouncer from './components/AccessibleNavigationAnnouncer';
+import PrivateRoute from './components/login/PrivateRoute';
 
-function App() {
+const Layout = lazy(() => import('./layout/Layout'));
+const Login = lazy(() => import('./pages/Login'));
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ToastContainer />
+      <Router>
+        <AccessibleNavigationAnnouncer />
+        <Switch>
+          <Route path="/login" component={Login} />
+          {/* <Route path="/signup" component={SignUp} /> */}
+          {/* <Route path="/forgot-password" component={ForgetPassword} /> */}
+          {/* <Route path="/reset-password/:token" component={ResetPassword} /> */}
+
+          <PrivateRoute>
+            {' '}
+            <Route path="/" component={Layout} />
+          </PrivateRoute>
+          <Redirect exact from="/" to="/login" />
+        </Switch>
+      </Router>
+    </>
   );
-}
+};
 
 export default App;
