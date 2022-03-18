30a93ab111691a1e61c5450e28dea05ede3349d2
Nguyễn Ngọc Đức Huy <huy312100@gmail.com>
2022-03-18 12:45:35 +0700

    Finish paging

---
+++ eCommerce.CustomerSite/Controllers/Components/PagerViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using eCommerce.Shared.ViewModels.Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.CustomerSite.Controllers.Components
+{
+	public class PagerViewComponent : ViewComponent
+	{
+		public Task<IViewComponentResult> InvokeAsync(PagedResultBase list)
+		{
+			return Task.FromResult((IViewComponentResult)View("Default", list));
+		}
+	}
+}
+
